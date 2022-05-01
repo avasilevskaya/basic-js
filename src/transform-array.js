ee64ef4c9ccb74995aba9dba9a15a9c6b084f026
avasilevskaya <anst.vasilevskaya@gmail.com>
2022-05-01 09:53:47 +0300

    feat: solved 'transform array'

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,64 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error ("'arr' parameter must be an instance of the Array!");
+  } else {
+    let myArr = arr.slice();
+    const dscrdNext = '--discard-next';
+    const dscrdPrev = '--discard-prev';
+    const dblNext = '--double-next';
+    const dblPrev = '--double-prev';
+    console.log(myArr);
+
+    for (let elem of myArr) {
+      let n;
+      switch (elem) {
+        case dscrdNext:
+        n = myArr.indexOf(dscrdNext);
+        if (n !== -1) {
+          myArr[n+1] = null;
+          myArr.splice(n,1);
+        }
+        console.log(myArr);
+
+        case dscrdPrev:
+        n = myArr.indexOf(dscrdPrev);
+        if (n !== -1) {
+          myArr.splice(n,1)
+          if (n !== 0) {
+            myArr[n-1] = null;
+          }
+        }
+        console.log(myArr);
+
+        case dblNext:
+        n = myArr.indexOf(dblNext);
+        if (n !== -1) {
+          if (typeof(myArr[n+1]) === "undefined") {
+            myArr.splice(n,1)
+          } else {
+            myArr[n] = myArr[n+1];
+          }
+        }
+        console.log(myArr);
+
+        case dblPrev:
+        n = myArr.indexOf(dblPrev);
+        if (n !== -1) {
+          if (n == 0) {
+            myArr.splice(n,1)
+          } else {
+            myArr[n] = myArr[n-1];
+          }
+        }
+        console.log(myArr);
+      }
+    }
+  let myArrFinal = myArr.filter(el => el !== null);
+  return myArrFinal;
+  }
 }
 
 module.exports = {
