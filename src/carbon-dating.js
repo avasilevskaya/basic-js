fcc1feb539f9fbab8af8a459e09a5c37ac79e7fd
avasilevskaya <anst.vasilevskaya@gmail.com>
2022-05-01 15:14:30 +0300

    feat: solved 'carbon dating'

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,39 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function dateSample(strSampleActivity) {
+  if (typeof(strSampleActivity) !== "string") {
+    return false;
+  }
+  let sampleActivity = strSampleActivity * 1;
+  if (sampleActivity + "" !== strSampleActivity) {
+    return false;
+  }
+
+  const log2 = 693 / 1000;
+
+  if (sampleActivity > MODERN_ACTIVITY || sampleActivity <= 0) {
+    return false;
+  }
+
+  let countDecimals = function(value) {
+    if (Math.floor(value) === value){
+      return 1;
+    }
+    return value.toString().split(".")[1].length;
+  }
+
+  nDec = countDecimals(sampleActivity);
+  
+  activityLN = Math.log(( MODERN_ACTIVITY * nDec) / ( sampleActivity * nDec));
+
+  nDecLN = countDecimals(activityLN);
+
+  let res = Math.ceil((activityLN * nDecLN) * HALF_LIFE_PERIOD / (log2 * nDecLN));
+  
+  //let res = Math.ceil(Math.log(( sampleActivity * nDec) / (MODERN_ACTIVITY * nDec)) * HALF_LIFE_PERIOD / log2);
+  return res;
 }
 
 module.exports = {
